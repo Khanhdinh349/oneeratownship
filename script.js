049f78618cb5d59126b20ff99be9ac7cee28dc7b
Felix Nghiem <47719265+Khanhdinh349@users.noreply.github.com>
2026-04-17 08:18:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const MAX_PER_REGISTRATION = 10;    // Tối đa 10 người cho 1 lần đăng 
 const APPSSCRIPT_URL = "https://script.google.com/macros/s/AKfycbzwNPeNr19fJr7hpO57m222AtX9cGisM0SVQydmofrd0RmoiDS7K4eGz6TVJYnz908YuQ/exec";
 
 const LOCK_CONFIG = {
-  // "2026-04-16": [""], 
+  "2026-04-17": ["09:00-10:30"], 
   "2026-04-25": ["09:00-10:30", "10:30-12:00","14:30-16:00","13:00-14:30"], // Khóa toàn bộ ngày
   "2026-04-26": ["09:00-10:30", "10:30-12:00","14:30-16:00","13:00-14:30"], // Khóa toàn bộ ngày
   "2026-04-27": ["09:00-10:30", "10:30-12:00","14:30-16:00","13:00-14:30"], // Khóa toàn bộ ngày
